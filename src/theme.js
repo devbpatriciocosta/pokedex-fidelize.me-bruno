dd3864264e1cb826d310c3e9eaf1eac8d8c09d2e
Bruno Patrício <bpatriciocosta@gmail.com>
2022-11-10 14:51:25 -0300

    Config Theme

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  primary: '#81DECF',
+  secondary: 'rgba(101, 220, 199, 0.3)',
+  background: '#FFFFFF',
+  secondBackgroundColor: '#ECECEC',
+  buttonColor: 'rgba(101, 220, 199, 0.8)',
+  buttonHover: 'rgba(11, 220, 199, 0.8)',
+  error: '#F70000'
+}
+
+export default theme
