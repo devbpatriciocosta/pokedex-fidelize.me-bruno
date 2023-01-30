6a4a58eeb849d79efb964679b2ca5929f3ef327c
Bruno Patrício <bpatriciocosta@gmail.com>
2023-01-30 14:26:28 -0300

    Grid Adjustments

--- src/components/pokedex/Pokedex.jsx
+++ src/components/pokedex/Pokedex.jsx
@@ -10,12 +10,12 @@ const PokedexContainer = styled.div`
   margin-top: 20px;
   grid-column-gap: 45px;
   grid-row-gap: 43px;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
 
   @media (max-width: 857px) {
     grid-row-gap: 10px;
     grid-column-gap: 10px;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   }
 
   @media (max-width: 689px) {
